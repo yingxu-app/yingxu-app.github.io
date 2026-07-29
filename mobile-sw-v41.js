fd6b856f85c68462be2504d3115bc1e7af995870
南域 <luguanlin20050927@icloud.com>
2026-07-29 17:43:27 +0800

    site: publish v41 for macOS v1.0.30

---
+++ mobile-sw-v41.js
@@ -0,0 +1,28 @@
+const CACHE = 'yingxu-pocket-v40';
+const ASSETS = [
+  './mobile-v41.html',
+  './mobile-manifest-v41.webmanifest',
+  './yingxu-pwa-icon.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(
+    keys.filter((key) => key.startsWith('yingxu-pocket-') && key !== CACHE)
+      .map((key) => caches.delete(key))
+  )));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then((response) => {
+    const copy = response.clone();
+    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
